726175d9973522835f667f0a3aba5123eab5abe7
D <some_random_mail@sus.com>
2024-11-06 16:28:59 +0300

    beer new fields, beer_color

--- models/beer.py
+++ models/beer.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, DECIMAL
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DECIMAL, Boolean
 from sqlalchemy.orm import relationship
 
 from app.database import Base
@@ -13,7 +13,14 @@ class Beer(Base):
     description = Column(String(250))
     photo = Column(String(100))
     alc_degree = Column(DECIMAL(2, 2), nullable=False)
+    ibu = Column(Integer)  # Международная шкала горечи IBU
+    og = Column(DECIMAL(5, 3))  # Исходная плотность OG
+    fg = Column(DECIMAL(5, 3))  # Конечная плотность FG
+    barrel_aged = Column(Boolean, default=False)  # Выдержка BA
+
     type_id = Column(Integer, ForeignKey('type_of_beers.id'), nullable=False)
+    color_id = Column(Integer, ForeignKey('colors.id'), nullable=True)
 
     type = relationship('TypeOfBeer')
+    color = relationship('BeerColor')
     places = relationship('Place', secondary='beers_places', back_populates='beers')
