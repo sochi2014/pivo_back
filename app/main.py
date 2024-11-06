9fa1cf4b86136a116506df559870c319acabca7e
D <some_random_mail@sus.com>
2024-11-06 22:00:15 +0300

    beer routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from .api.v1 import test_routes, auth_api, users_api
+from .api.v1 import test_routes, auth_api, users_api, beer_routes
 from .database import engine, Base
 
 Base.metadata.create_all(bind=engine)
@@ -10,3 +10,5 @@ app = FastAPI()
 app.include_router(test_routes.router, tags=["pivo"])
 app.include_router(auth_api.router, tags=["auth"])
 app.include_router(users_api.router, tags=["user"])
+
+app.include_router(beer_routes.router, tags=["beer"])
