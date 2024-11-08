9f80c5f7c639410fb6d9428a2316b049e7f5da51
Neimess <113237814+Neimess@users.noreply.github.com>
2024-11-08 12:44:10 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@
 echo "Running Alembic migrations..."
 python -m alembic upgrade head
 
-echo "DUMP"
-sqlite3 database.db < database.sql
-
 echo "Start docker-compose"
 docker compose up -d --build
 echo "Deployment completed successfully!"
